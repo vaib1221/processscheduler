bd38c82fd9ee46c937b32f994a0d6a8be70dbf6a
tare2.galal <tare2.galal@fe2f2eb8-3e63-11de-8950-77ccfa13e4cc>
2009-05-11 21:48:19 +0000

    Done: Shortest Time Remaining

--- trunk/Scheduler/src/scheduler/Main.java
+++ trunk/Scheduler/src/scheduler/Main.java
@@ -20,34 +20,85 @@ public class Main {
        
        for(int i=0; i<processes.length; i++)
        {
-           processes[i]=new Process("P"+i,3, i, "tarek");
+           processes[i]=new Process("P"+(i+1),3, i, "tarek");
        }
        
+        processes[0].name="A";  processes[0].Ta=0;  processes[0].Tr=3;
+        processes[1].name="B";  processes[1].Ta=1;  processes[1].Tr=5;
+        processes[2].name="C";  processes[2].Ta=3;  processes[2].Tr=2;
+        processes[3].name="D";  processes[3].Ta=7;  processes[3].Tr=2;
+        processes[4].name="E";  processes[4].Ta=10; processes[4].Tr=5;
        
        
        startShortestTimeRemaining(processes);
-     
+       
+       
+     //System.out.println(getShortestTimeRemainingProcess(processes, 4, processes[0]).name);
         
     }
     
-    public static Process getShortestTimeRemaining(Process p)
+    public static Process getShortestTimeRemainingProcess(Process []p, int T, Process currentP) //returns the process allowed to run
     {
         
+        //4 2 6 7
         
-    return p;    
+        Process shortest;
+      /* for(int i=0;i<p.length; i++)
+       {
+            
+           if(p[i].Ta<=T)   //process is available, check   shortest length
+           {*/
+              shortest=currentP;
+              
+               for(int j=0; j<p.length; j++) //get minumum
+               {
+                   if(shortest.Tr<0 && p[j].Ta<=T && p[j].Tr>-1)
+                       shortest=p[j];
+                      
+                           
+                   
+                   if(p[j].Tr<shortest.Tr && p[j].Ta<=T && p[j].Tr>-1)
+                       shortest=p[j];
+                   
+               }
+               
+           //}
+       //}
+       
+       
+       return shortest;
     }
     
     public static void startShortestTimeRemaining(Process[] p)
     {
         
+        // System.out.println(getShortestTimeRemainingProcess(p, 3, p[0]).name);
+        
+        
         int totalTime=0;
         
-        for(int i=0; i<p.length; i++)
+        for(int i=0; i<p.length; i++)   //Get total time
         {
             totalTime+=p[i].Tr;
         }
         
-        System.out.println(totalTime);
+        Process current=p[0];
+        
+        for(int i=0; i<totalTime; i++)   //TimeLine
+        {
+           current=getShortestTimeRemainingProcess(p, i, current);
+          
+           boolean run;
+           
+           if(!(run=current.run()))
+               i--;
+           else
+           //System.out.print(i+": "+current.name+""+run+"   ");
+           System.out.print(i+": "+current.name+"    ");
+          
+            
+        }
+       
         
     }
 
