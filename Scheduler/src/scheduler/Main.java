37c287b05b05ed207801be744037fdb58125d00f
tare2.galal <tare2.galal@fe2f2eb8-3e63-11de-8950-77ccfa13e4cc>
2009-05-12 00:57:02 +0000

    Multilevel Feedback Problems with queue q2!!

--- Scheduler/src/scheduler/Main.java
+++ Scheduler/src/scheduler/Main.java
@@ -33,7 +33,9 @@ public class Main{
         processes[4].name="E";  processes[4].Ta=10; processes[4].Tr=5;
        
        
-       startShortestTimeRemaining(processes);
+      // startShortestTimeRemaining(processes);
+        
+        startMultilevelFeedback(processes);
        
        
      //System.out.println(getShortestTimeRemainingProcess(processes, 4, processes[0]).name);
@@ -109,16 +111,17 @@ public class Main{
        
         
     }
+   
+        static Queue <Process> running = new Queue<Process>();
+       static Queue <Process> q0 = new Queue<Process>();
+       static Queue <Process> q1 = new Queue<Process>();
+       static Queue <Process> q2 = new Queue<Process>();
+       static Queue <Process> q3 = new Queue<Process>();
     
     public static void startMultilevelFeedback(Process[] p)
     {
         
-        Queue <Process> q0 = new Queue<Process>();
-        Queue <Process> q1 = new Queue<Process>();
-        Queue <Process> q2 = new Queue<Process>();
-        Queue <Process> q3 = new Queue<Process>();
-        
-        
+
         int totalTime=0;
         
         for(int i=0; i<p.length; i++)   //Get total time
@@ -126,40 +129,106 @@ public class Main{
             totalTime+=p[i].Tr;
         }
         
-        q3.enqueue(p[3]);
-        q1.enqueue(p[0]);
-        q1.enqueue(p[1]);
-        q2.enqueue(p[2]);
-        q0.enqueue(p[4]);
-        
-        
-    /*    for(int i=0; i<totalTime; i++)
+                for(int i=0; i<totalTime; i++) //print timeline
+        {
+           System.out.print(i+"\t"); 
+        }
+            System.out.println("\n");
+  /////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////
+            
+        int count=totalTime;
+        for(int i=0; i<totalTime; i++) ///////populate time line
         {
             
-            for(int=0; i<p.length; i++)
+            
+            for(int j=0; j<p.length; j++)  //check if any process has arrived at this instance
             {
-                if(i==p[i].Ta){
-                    q1.enqueue(p[i]);
-                    p[i].quantum=1;
+                if(i==p[j].Ta){
+                    q0.enqueue(p[j]);
+                    //System.out.println("at t="+i+" "+p[j].name+" has been enqueued");
+                    p[j].setQuantum(1);
                 }
             }
             
             
-        }*/
+                                            //start the actual work
+            Process toRun;
+            if((toRun=running.dequeue())==null)
+                toRun=getQueuedProcess();
+            
+           //if(toRun.name.equals("E"))
+                //System.out.println("E===> "+toRun.quantum+" and B===>"+p[1].quantum);
+            
+            
+            
+            if(toRun.quantum>0)
+            {
+               
+              if(toRun.run())
+              {
+                  System.out.print(toRun.name+"\t");
+                 
+                  
+                  // count--;
+ 
+                 toRun.quantum--;  
+              }
+              else{
+                  toRun.quantum=0;
+                  i--;
+                 // break;
+              }
+              
+              
+            }
+            
+            
+            
+            if(toRun.quantum==0)
+            {
+            switch(toRun.initQuantum)
+            {
+                case 1: q1.enqueue(toRun); toRun.setQuantum(2); break; 
+                case 2: q2.enqueue(toRun); toRun.setQuantum(4); /*if(toRun.name.equals("B") || toRun.name.equals("E")) System.out.println("WILL PUTTT "+toRun.name);*/ break;
+                case 4: q3.enqueue(toRun); toRun.setQuantum(8); break;
+                case 8: q3.enqueue(toRun); break;
+            }
+            
+            }
+            else
+            {
+                //if(toRun.Tr>-1)
+                    if(toRun.name.equals("E"))
+                    System.out.print("W\t");
+                running.enqueue(toRun);
+            }
+            
+             
         
-        System.out.println(getQueuedProcess(q0,q1,q2,q3).name);
+        }
+        
+        //System.out.println(getQueuedProcess().name);
+            
+           
         
     }
     
-    public static Process getQueuedProcess(Queue<Process> q0, Queue<Process> q1, Queue<Process> q2,Queue<Process> q3)
+    
+    
+    public static Process getQueuedProcess()
     {
         Process toRun;
         
         if((toRun=q0.dequeue())==null)
             if((toRun=q1.dequeue())==null)
                 if((toRun=q2.dequeue())==null)
-                    if((toRun=q3.dequeue())==null)
-                        return toRun;
+                    if((toRun=q3.dequeue())==null);
+                        
+        
+        
+       
         
         return toRun;
     }
