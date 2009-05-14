5b58d067d70c73ae7752705c72c52bc86461ed61
tare2.galal <tare2.galal@fe2f2eb8-3e63-11de-8950-77ccfa13e4cc>
2009-05-14 15:42:47 +0000

    Starting Lottery Scheduling

---
+++ trunk/Scheduler/src/scheduler/User.java
@@ -0,0 +1,25 @@
+package scheduler;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tarek
+ */
+public class User {
+    
+    String username;
+    int priority;
+    
+    public User(String username, int priority)
+    {
+        this.username=username;
+        this.priority=priority;
+    }
+    
+    
+
+}
