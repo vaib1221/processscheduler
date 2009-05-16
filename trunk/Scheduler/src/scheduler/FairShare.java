a8290413d630d2b188948c040d9da8602f98b888
tare2.galal <tare2.galal@fe2f2eb8-3e63-11de-8950-77ccfa13e4cc>
2009-05-16 17:52:17 +0000

    Fairshare Scheduling\\ initialized processes and users

--- trunk/Scheduler/src/scheduler/FairShare.java
+++ trunk/Scheduler/src/scheduler/FairShare.java
@@ -10,5 +10,53 @@ package scheduler;
  * @author tarek
  */
 public class FairShare {
+    
+    User bigShot, fairEnough, notBad, poorThing;
+    
+    
+    Process[] processes=new Process[8];
+
+    public FairShare() {  
+
+    initUsers();
+    initProcesses();  
+
+    }
+    
+    public void initUsers()
+    {
+        
+        this.bigShot = new User("BigShot", 50);
+        this.fairEnough = new User("FairEnough", 25);
+        this.notBad = new User("NotBad", 20);
+        this.poorThing = new User("PoorThing", 5);
+        
+    }
+    
+    public void initProcesses()
+    {
+        
+         for(int i=0; i<processes.length; i++)
+       {
+           processes[i]=new Process();
+        }
+
+        this.processes[0] = new Process("A", 2, 0, bigShot);
+        this.processes[1] = new Process("B", 2, 0, bigShot);
+        this.processes[2] = new Process("C", 2, 0, fairEnough);
+        this.processes[3] = new Process("D", 2, 0, fairEnough);
+        this.processes[4] = new Process("E", 2, 0, fairEnough);
+        this.processes[5] = new Process("F", 2, 0, notBad);
+        this.processes[6] = new Process("G", 2, 0, notBad);
+        this.processes[7] = new Process("H", 2, 0, poorThing);
+        
+        
+        
+    }
+    
+    public void start()
+    {
+       
+    }
 
 }
